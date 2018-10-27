5b3a9a31d8f900d34c7587f73caedfbab38d4093
Riolu <hankunyan68@gmail.com>
2018-10-27 17:29:33 -0400

    modify the db

--- flowey-server/flowey/models.py
+++ flowey-server/flowey/models.py
@@ -5,9 +5,9 @@ class User(db.Model):
     pass
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    username = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
 
     def __init__(self, username, password, email):
         self.username = username
@@ -16,5 +16,5 @@ class User(db.Model):
 
     def __repr__(self):
         #return '<User %r>' % self.username
-        return "id:{} username:{} password:{} email:{}".format(self.id, self.password, self.password, self.email)
+        return "id:{} username:{} password:{} email:{}".format(self.id, self.username, self.password, self.email)
 
