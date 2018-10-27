4fb6b6d569f4fa6c7b1a1ae112ed67c95bcb26b9
Riolu <hankunyan68@gmail.com>
2018-10-27 17:25:31 -0400

    add register

--- flowey-server/flowey/api/auth.py
+++ flowey-server/flowey/api/auth.py
@@ -3,28 +3,30 @@ from flowey.models import User
 from flowey.ext import db
 from flask_jwt_extended import create_access_token
 from werkzeug.security import safe_str_cmp
+from flask import jsonify
 
 api = Namespace('auth', description='authentication APIs')
 
 
-@api.route('/register')
 class Register(Resource):
-    @api.doc('test api hello')
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', type=str, required=True, help='name to create user')
+    parser.add_argument('password', type=str, required=True, help='password to create user')
+    parser.add_argument('email', type=str, required=True, help='email to create user')
+
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('username', type=str, help='name to create user')
-        parser.add_argument('password', type=str, help='password to create user')
-        parser.add_argument('email', type=str, help='email to create user')
-        args = parser.parse_args()
+        args = self.parser.parse_args()
+        tmp = User.query.filter_by(email=args['email']).first()
 
-        print(args)
+        if tmp: # already exists
+            return {"message": "Email Already Exists"}, 403
+        else:
+            new_user = User(args['username'], args['password'], args['email'])
+            db.session.add(new_user)
+            db.session.commit()  # This is needed to write the changes to database
+            #print(User.query.all()) # list type
+            return {"message": "Register Success!"}, 500
 
-        new_user = User(args['username'], args['password'], args['email'])
-        db.session.add(new_user)
-        db.session.commit()  # This is needed to write the changes to database
-        print(User.query.all()) # list type
-
-        return "Post"
 
 
 @api.route('/login')
@@ -45,7 +47,7 @@ class Login(Resource):
     def post(self):
         data = self.parser.parse_args()
         # read from database to find the user and then check the password
-        user = User.query.filter_by(email=data['email']).first()
+        user = User.query.filter_by(username=data['email']).first()
 
         if user and safe_str_cmp(user.password, data['password']):
             # when authenticated, return a fresh access token and a refresh token
@@ -63,3 +65,11 @@ class Login(Resource):
         return {"message": "Invalid Credentials!"}, 401
 
 
+
+@api.route('/show')
+class Show(Resource):
+    def get(self):
+        data = User.query.all()
+        result = [{'username':x.username, 'password':x.password, 'email':x.email} for x in data]
+        #print(result)
+        return jsonify(result)
