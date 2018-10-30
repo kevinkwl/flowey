51173bb8df5b6ff10fddc090bd5afbaf5297c596
Zefeng-Liu <zl2715@columbia.edu>
2018-10-29 22:15:29 -0400

    support 'flask test --coverage'

--- flowey-server/flasky.py
+++ flowey-server/flasky.py
@@ -1,10 +1,18 @@
 import os
+import sys
+import click
 from flowey import create_app, db
 from flowey.models import User
 from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='flowey/*')
+    COV.start()
 
 
 @app.shell_context_processor
@@ -13,8 +21,23 @@ def make_shell_context():
 
 
 @app.cli.command()
-def test():
+@click.option('--coverage/--no-coverage', default=False,
+              help='Run tests under code coverage.')
+def test(coverage):
     """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
