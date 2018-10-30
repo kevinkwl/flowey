80022b3003bc0b7442958bdb2738d10cbc324409
Xi Yang <xi.y@columbia.edu>
2018-10-29 22:19:47 -0400

    minor adjustments

--- flowey-server/flasky.py
+++ flowey-server/flasky.py
@@ -2,6 +2,7 @@ import os
 from flowey import create_app, db
 from flowey.models import User
 from flask_migrate import Migrate
+from scripts.sample_data import SampleData
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -22,9 +23,8 @@ def test():
 
 @app.cli.command()
 def sampledata():
-    from sample_data import SampleData
     sd = SampleData()
     sd.add_users()
-    sd.del_users()
     sd.add_transactions()
     sd.del_transactions()
+    sd.del_users()
