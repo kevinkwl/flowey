d22e0f2bab31bae778d26779a2f435d76ca436ae
Kangwei Ling <lingkangwei.kevin@gmail.com>
2018-11-01 13:54:41 -0400

    echo in test

--- test.sh
+++ test.sh
@@ -6,4 +6,5 @@ pylint --disable=C flowey-server -f colorized > reports/pylint_result.txt
 cd flowey-server
 FLASK_APP=flasky.py flask test --coverage > ../reports/test_with_coverage.txt 2>&1
 cd ..
+echo "test done"
 
