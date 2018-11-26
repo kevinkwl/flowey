1324013c6a0d1f6f761fe5de3bf804c8dd79b493
Kangwei Ling <lingkangwei.kevin@gmail.com>
2018-11-25 21:41:06 -0500

    add friends models

--- flowey-server/flowey/models.py
+++ flowey-server/flowey/models.py
@@ -10,12 +10,40 @@ def json_serial(obj):
     return obj
 
 
+friends = db.Table(
+    'friends',
+    db.Column('uid', db.Integer, db.ForeignKey('users.id')),
+    db.Column('fid', db.Integer, db.ForeignKey('users.id'))
+)
+
+friend_reqs = db.Table(
+    'friend_reqs',
+    db.Column('uid', db.Integer, db.ForeignKey('users.id')),
+    db.Column('fid', db.Integer, db.ForeignKey('users.id'))
+)
+
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
+
+    friends = db.relationship("User",
+                              secondary=friends,
+                              primaryjoin=(friends.c.uid == id),
+                              secondaryjoin=(friends.c.fid == id),
+                              backref=db.backref('friend_to', lazy='dynamic'),
+                              lazy='dynamic')
+
+    friend_requested = db.relationship("User",
+                                       secondary=friend_reqs,
+                                       primaryjoin=(friend_reqs.c.uid == id),
+                                       secondaryjoin=(friend_reqs.c.fid == id),
+                                       backref=db.backref('friend_received',
+                                                          lazy='dynamic'),
+                                       lazy='dynamic')
 
     def __init__(self, username, password, email):
         self.username = username
@@ -27,6 +55,50 @@ class User(db.Model):
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def has_friend(self, user):
+        return self.friends.filter(friends.c.fid == user.id).count() > 0
+
+    def add_friend(self, user):
+        if not self.has_friend(user):
+            self.friends.append(user)
+            self.friend_to.append(user)
+            return self
+
+    def remove_friend(self, user):
+        if self.has_friend(user):
+            self.friends.remove(user)
+            self.friend_to.remove(user)
+            return self
+
+    def has_friend_request_to(self, user):
+        return self.friend_requested.filter(friend_reqs.c.fid == user.id) \
+                .count() > 0
+
+    def has_friend_request_from(self, user):
+        return self.friend_received.filter(friend_reqs.c.uid == user.id) \
+                .count() > 0
+
+    @staticmethod
+    def exist_friend_request_between(user1, user2):
+        return user1.has_friend_request_to(user2) or \
+                user2.has_friend_request_to(user1)
+
+    def send_friend_request_to(self, user):
+        self.friend_requested.append(user)
+        db.session.add(self)
+        db.session.commit()
+
+    def agree_friend_request_from(self, user):
+        if self.has_friend_request_from(user):
+            self.friend_received.remove(user)
+            self.add_friend(user)
+            db.session.add(self)
+            db.session.commit()
+
+    def get_friends(self):
+        return [user.username for user in self.friends]
+
 
 
 class Transaction(db.Model):
@@ -71,3 +143,7 @@ class TokenBlackList(db.Model):
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+
+
---
+++ flowey-server/tests/test_friends.py
@@ -0,0 +1,54 @@
+import unittest
+from flowey.app import create_app
+from flowey.ext import db
+from flowey.models import User
+from tests.utils import login
+import json
+
+
+class FriendsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        self.client = self.app.test_client(use_cookies=True)
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_basic_friends(self):
+        user1 = User('f1', 'f1', 'f1')
+        user2 = User('f2', 'f2', 'f2')
+
+        db.session.add(user1)
+        db.session.commit()
+        db.session.add(user2)
+        db.session.commit()
+
+        users = User.query.all()
+
+        assert user1 in users and user2 in users
+
+        self.assertEqual(len(user1.get_friends()), 0)
+        self.assertEqual(len(user1.get_friends()), 0)
+
+        user1.send_friend_request_to(user2)
+
+        self.assertTrue(user1.has_friend_request_to(user2))
+        self.assertTrue(user2.has_friend_request_from(user1))
+
+        self.assertTrue(User.exist_friend_request_between(user1, user2))
+
+        user2.agree_friend_request_from(user1)
+
+        self.assertFalse(user1.has_friend_request_to(user2))
+        self.assertEqual(len(user1.get_friends()), 1)
+        self.assertEqual(len(user2.get_friends()), 1)
+
+        self.assertEqual(user1.get_friends()[0], user2.username)
+        self.assertEqual(user2.get_friends()[0], user1.username)
+
+
